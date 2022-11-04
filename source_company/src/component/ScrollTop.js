acb782fd8d8d1addb0fad8b5ae15e869236d5b88
SongMiok <86095532+aluvy@users.noreply.github.com>
2022-11-04 23:52:09 +0900

    scrolltop 버그 수정

--- source_company/src/component/ScrollTop.js
+++ source_company/src/component/ScrollTop.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import * as common from "./../script.js";
 
 function ScrollTop(){
 	let [scroll, setScroll] = useState('');
@@ -18,7 +19,7 @@ function ScrollTop(){
     }, []);
 
 	return (
-		<div className={`btn__scrolltop ${scroll}`}>
+		<div className={`btn__scrolltop ${scroll}`} onClick={common.scrollTop}>
 			<svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="KeyboardArrowUpIcon" fill="#fff">
 				<path d="M7.41 15.41 12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path>
 			</svg>
